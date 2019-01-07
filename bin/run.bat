c0c6f3dfb2ae1126812433097fc1ad7823d2d2a9
Joe Bian <bianjiudong@gmail.com>
2019-01-06 19:31:00 -0500

    Changes to be committed: 	modified:   bin/run.bat to add command in bat file for demo purpose

--- bin/run.bat
+++ bin/run.bat
@@ -1,1 +1,4 @@
 mvn compiler:compile robotframework:run -Dlog4j2.configurationFile=%cd%/conf/log4j2.xml
+mvn clean package -DskipTests=true
+java -cp "lib/*" -Dlog4j2.configurationFile=./conf/log4j2.xml org.robotframework.RobotFramework ./testcases/demo.robot
+taskkill /F /IM chromedriver.exe
